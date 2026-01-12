3814cca621dabf3d4a815a640f360345ec19c7ed
emp3r0r7 <ichigochris@hotmail.it>
2026-01-12 23:12:46 +0100

    fixed behaviour that caused no node signal level when application is brought in foreground after sleep time for direct nodes

--- app/src/main/java/com/geeksville/mesh/ui/components/SignalInfo.kt
+++ app/src/main/java/com/geeksville/mesh/ui/components/SignalInfo.kt
@@ -21,6 +21,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
@@ -72,6 +73,15 @@ fun SignalInfo(
     if (node.hopsAway <= 0) {
         if (node.snr < MAX_VALID_SNR && node.rssi < MAX_VALID_RSSI) {
             NodeSignalQuality(node.snr, node.rssi)
+        } else {
+            //not showing direct for our node!
+            if (isThisNode) return
+            Text(
+                modifier = modifier,
+                text = "Direct Node",
+                color = Color.Green,
+                fontSize = MaterialTheme.typography.button.fontSize
+            )
         }
     }
 }
