badf7813c6d8ae28e7d27193cfea8028d160ba47
emp3r0r7 <ichigochris@hotmail.it>
2026-04-11 10:18:57 +0200

    chore: updated to proto v2.7.21 and device hardware.json

--- app/src/main/java/com/geeksville/mesh/MainActivity.kt
+++ app/src/main/java/com/geeksville/mesh/MainActivity.kt
@@ -1327,7 +1327,7 @@ class MainActivity : AppCompatActivity(), Logging {
 
             //val packageInfo: PackageInfo = packageManager.getPackageInfoCompat(packageName, 0)
             //val versionName = packageInfo.versionName
-            Toast.makeText(this, "$gitSha\nProto v2.7.20", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "$gitSha\nProto v2.7.21", Toast.LENGTH_LONG).show()
         } catch (e: PackageManager.NameNotFoundException) {
             errormsg("Can not find the version: ${e.message}")
         }
